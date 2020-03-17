5e776f6ac26da99987b3c7878622bbf353bce805
sunnypoy <73930126@qq.com>
2020-03-17 14:37:47 +0800

    更新

--- src/main/java/com/example/Math.java
+++ src/main/java/com/example/Math.java
@@ -10,4 +10,5 @@ public class Math {
     public int multiply(int a,int b){
         return a*b;
     }
+
 }
---
+++ src/main/java/com/example/helloworld.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("我的第一个Java程序：Hello World。");
+    }
+}
